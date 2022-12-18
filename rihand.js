548a3a03ac24036ac97f4252d6998a739c625775
rihandcreative <91003498+rihandcreative@users.noreply.github.com>
2022-12-18 07:18:31 +0700

    Initial Commit

---
+++ rihand.js
@@ -0,0 +1,6 @@
+document.body.setAttribute('oncontextmenu', 'return false');
+document.body.setAttribute('onselectstart', 'return false');
+document.body.setAttribute('onCopy', 'return false');
+document.body.setAttribute('onCut', 'return false');
+document.body.setAttribute('onDrag', 'return false');
+document.body.setAttribute('onDrop', 'return false');
